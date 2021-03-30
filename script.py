6c4b77d88ec84b2bd9ac3b9a3bc3baf7efb8a326
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-03-30 13:45:54 +0200

    added converting mp4 to mp3 and writing to a file

--- script.py
+++ script.py
@@ -1,34 +1,33 @@
 import os
+import shutil
 from pydub.silence import split_on_silence
 from pydub import AudioSegment
 import speech_recognition as sr
+import ffmpeg
+
+
+command2mp3 = "ffmpeg -i speech.mp4 speech.mp3"
+command2wav = "ffmpeg -i speech.mp3 speech.wav"
 
 """ 
 TODO: 
-1.konwersja z mp4 na wav/mp3 
+1.konwersja z mp4 na wav 
 2.large file splitting
 3. writting to .txt file
  """
 
-filename = "./audio_test/Intro_read.wav"
 
 r = sr.Recognizer()
 
-with sr.AudioFile(filename) as source:
-    # listen for the data (load audio to memory)
-    audio_data = r.record(source)
-    # recognize (convert from speech to text)
-    text = r.recognize_google(audio_data, language="PL")
-    print(text)
+os.mkdir('./tmp')
 
+# converting mp4 file into wav file
+filename = "./video_test/test-1.mp4"
+output = "./tmp/temp.wav"
 
-# importing libraries
-
-# create a speech recognition object
-r = sr.Recognizer()
-
-# a function that splits the audio file into chunks
-# and applies speech recognition
+stream = ffmpeg.input('./video_test/test-1.mp4')
+stream = ffmpeg.output(stream, output)
+ffmpeg.run(stream)
 
 
 def get_large_audio_transcription(path):
@@ -41,7 +40,7 @@ def get_large_audio_transcription(path):
     # split audio sound where silence is 700 miliseconds or more and get chunks
     chunks = split_on_silence(sound,
                               # experiment with this value for your target audio file
-                              min_silence_len=500,
+                              min_silence_len=1000,
                               # adjust this per requirement
                               silence_thresh=sound.dBFS-14,
                               # keep the silence for 1 second, adjustable as well
@@ -56,14 +55,14 @@ def get_large_audio_transcription(path):
     for i, audio_chunk in enumerate(chunks, start=1):
         # export audio chunk and save it in
         # the `folder_name` directory.
-        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        chunk_filename = os.path.join(folder_name, f"chunk{i}")
         audio_chunk.export(chunk_filename, format="wav")
         # recognize the chunk
         with sr.AudioFile(chunk_filename) as source:
             audio_listened = r.record(source)
             # try converting it to text
             try:
-                text = r.recognize_google(audio_listened)
+                text = r.recognize_google(audio_listened, language="PL")
             except sr.UnknownValueError as e:
                 print("Error:", str(e))
             else:
@@ -72,3 +71,17 @@ def get_large_audio_transcription(path):
                 whole_text += text
     # return the text for all chunks detected
     return whole_text
+
+
+text = get_large_audio_transcription(output)
+print(text)
+
+file = open('output.txt', 'w+', encoding='utf-8')
+file.write(text)
+
+shutil.rmtree('./audio-chunks')
+shutil.rmtree('./tmp')
+
+
+# a function that splits the audio file into chunks
+# and applies speech recognition
